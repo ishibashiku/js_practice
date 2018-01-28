778e9a6bc8d2315d9c76bc73e3e00df9e0314c51
ishibashiku <ishibashi.k1992@gmail.com>
2018-01-29 00:49:21 +0900

    ディレクトリ構成変更

--- dic/jQuery-ex2/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    // 一旦全てのddタグをhiddenにする
-    $('#accordion dd').hide();
-    // dtタグをクリックされた時のddタグの挙動
-    $('#accordion dt').click(function(e){
-        $(this).next('dd').slideToggle()
-    });
-});
